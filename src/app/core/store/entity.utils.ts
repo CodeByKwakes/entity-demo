2bca56d6264896b0c206caf91ba97a5d7648916a
Kwaku Prempeh <kwaku.prempeh@holition.com>
2018-06-25 13:57:41 +0100

    still working. entities created

--- src/app/core/store/entity.utils.ts
+++ src/app/core/store/entity.utils.ts
@@ -1,13 +1,22 @@
-export interface EntityState<V> {
+import { Payload } from '../models/data-api';
+
+export declare interface DictionaryNum<T> {
+  [key: number]: T;
+}
+
+export declare abstract class Dictionary<T> implements DictionaryNum<T> {
+  [key: string]: T;
+}
+
+export interface EntityState<T> {
   ids: string[] | number[];
-  entities: { [key: string]: V };
+  entities: Dictionary<T>;
 }
 
 export const createEnitites = (array: any[], keyField, state?) => {
   const ids = array.map(item => item[keyField]);
-  const entities: {
-    [key: number]: any;
-  } = {};
+
+  const entities: Dictionary<Payload> = {};
   array.forEach(item => {
     entities[item[keyField]] = item;
   });
